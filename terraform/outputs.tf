90ac44a2c8f6fc9a2e19d514f224cb9b36b7079d
Ofir Yakobi <ofir.yakobi@orca.security>
2024-07-28 22:32:23 +0300

    removed outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,18 +6,10 @@ output "backend_url" {
   value = module.webserver.backend_url
 }
 
-output "rds_endpoint" {
-  value = module.webserver.rds_endpoint
-}
-
 output "bucket_url" {
   value = module.front.bucket_url
 }
 
 output "rds_address" {
   value = module.webserver.rds_endpoint
-}
-
-output "output_integrity_api_gateway_url" {
-  value = module.output_integrity.api_invoke_url
 }
