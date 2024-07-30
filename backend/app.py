74e88da63a377142c64289af9f1aa8e5cf8d1f3d
Ofir Yakobi <ofir.yakobi@orca.security>
2024-07-30 13:12:16 +0300

    added hints api calls and removed comments

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,6 @@ from functools import wraps
 import jwt
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify, current_app
 import json
-# from flask_sqlalchemy import SQLAlchemy
 from models import Product, Category, User, db, Comment, product_categories
 import boto3
 from botocore.exceptions import NoCredentialsError
@@ -21,15 +20,12 @@ from sqlalchemy.dialects.postgresql import JSONB
 from flask_cors import CORS
 
 
-# Set up logging
 log_handler = logging.StreamHandler()
 log_handler.setLevel(logging.INFO)
 
-# Set a formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log_handler.setFormatter(formatter)
 
-# Ensure the handler flushes after each log entry
 class FlushHandler(logging.StreamHandler):
     def emit(self, record):
         super().emit(record)
@@ -46,34 +42,29 @@ parser.add_argument('--similar_images_api_gateway', type=str, help='Similar imag
 parser.add_argument('--similar_images_bucket', type=str, help='Similar images bucket name')
 parser.add_argument('--get_recs_api_gateway', type=str, help='Get user recommendations api gateway URL')
 parser.add_argument('--data_poisoning_bucket', type=str, help='Data Poisoning bucket name')
+
 args = parser.parse_args()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a_secret_key_that_you_should_change'
-# Set maximum file size to 16MB
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-CORS(app, resources={r"/*": {"origins": "*"}})  # This will enable CORS for all routes
-# Setup logging
+CORS(app, resources={r"/*": {"origins": "*"}})
 logging.basicConfig(level=logging.DEBUG)
 app.logger.addHandler(FlushHandler())
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{args.db_user}:{args.db_password}@{args.db_host}:5432/{args.db_name}'
-# app.configEnv['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-# db = SQLAlchemy(app)
 s3 = boto3.client('s3')
 
-
-# Dummy user store
+# Dummy user data
 users = {
     'babyshark': 'doodoo123'
 }
 
 
-#Ofir change it:
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -93,87 +84,27 @@ def token_required(f):
 
 
 
-
-# def load_cart(user):
-#     # Assuming cart data is stored in a JSON file
-#     try:
-#         with open(f'cart.json') as f:
-#             return json.load(f)
-#     except FileNotFoundError:
-#         return []
-
 user_carts = {
     'babyshark': []
 }
 
 
-def load_cart(user):
-    return user_carts.get(user, [])
-
-def load_products():
-    products = Product.query.all()
-    return jsonify([p.to_dict() for p in products])
-
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'jpg', 'jpeg', 'png', 'gif'}
 
 
-# Helper function to find product by ID
 def get_product_by_id(product_id):
     return Product.query.get(product_id)
 
-
-# Helper function to find products by multiple IDs
 def get_products_by_ids(ids, products):
     id_list = [int(id_str) for id_str in ids.split(',')]
     result = [product for product in products if product.id in id_list]
     return result
 
-def get_products_by_category(category, products):
-    # category_id = int(category_id)
-    filtered_products = []
-
-    for product in products:
-        if any(category.id == category for category in product.categories):
-            filtered_products.append(product)
-
-    return filtered_products
-
-#
-# def get_category_by_id(category_id):
-#     for category in categories:
-#         if category['id'] == int(category_id):
-#             return category
-#     return None
-#
 
 @app.route('/')
 def home():
     return "Welcome to the AI Goat Store!"
-
-@app.route('/api/cart/add', methods=['POST'])
-@token_required
-def add_to_cart(current_user):
-    data = request.get_json()
-    cart = load_cart(current_user)
-    cart.append(data)
-    user_carts[current_user] = cart
-    return jsonify({'message': 'Item added to cart'})
-
-@app.route('/api/cart/remove', methods=['POST'])
-@token_required
-def remove_from_cart(current_user):
-    data = request.get_json()
-    cart = load_cart(current_user)
-    user_carts[current_user] = [item for item in cart if item['id'] != data['id']]
-    return jsonify({'message': 'Item removed from cart'})
-
-@app.route('/api/cart/update', methods=['POST'])
-@token_required
-def update_cart(current_user):
-    data = request.get_json()
-    # Update the product count in the user's cart (implementation details omitted)
-    return jsonify({'message': 'Cart updated'})
 
 
 @app.route('/api/recommendations', methods=['GET'])
@@ -181,7 +112,6 @@ def update_cart(current_user):
 def get_recommendations(current_user):
     user = User.query.filter_by(username=current_user).first_or_404()
 
-    # Invoke the SageMaker endpoint via the API Gateway
     request_payload = {
         "user_id": user.id
     }
@@ -197,10 +127,8 @@ def get_recommendations(current_user):
         recommended_products_ids = recommended_products_ids[:4]
         user.recommendations = recommended_products_ids
 
-        # Update the user's recommendations in the database
         db.session.commit()
         recommended_products = Product.query.filter(Product.id.in_(recommended_products_ids)).all()
-        # Debugging: Print the individual product IDs and their presence in the database
         for product_id in recommended_products_ids:
             product = Product.query.get(product_id)
             if product:
@@ -216,7 +144,6 @@ def get_recommendations(current_user):
 @token_required
 def get_cart(current_user):
     try:
-        # Fetch the user's cart items from the database
         user = User.query.filter_by(username=current_user.username).first()
         if not user:
             return jsonify({'message': 'User not found'}), 404
@@ -261,12 +188,9 @@ def find_similar_images(query_features, features, top_n=5):
 @app.route('/api/analyze-photo', methods=['OPTIONS', 'POST'])
 def product_lookup():
     if request.method == 'OPTIONS':
-        return '', 204  # Respond to preflight request with 204 No Content
+        return '', 204
 
     matched_products = []
-    # if 'user' not in session:
-    #     return redirect(url_for('login'))
-    # print('Request Files:', request.files)
     if request.method == 'POST':
         try:
             app.logger.info('Request Files: %s', request.__dict__)
@@ -285,7 +209,6 @@ def product_lookup():
         else:
             flash('Invalid file format.', 'error')
 
-        # Upload photo to S3 bucket
         bucket_name = args.similar_images_bucket
         api_endpoint = args.similar_images_api_gateway
 
@@ -303,7 +226,6 @@ def product_lookup():
             app.logger.error("Credentials not available")
             return jsonify({'error': 'Credentials not available'}), 500
 
-        # Process the image and execute metadata as a command if present
         with open(photo_path, 'rb') as img_file:
             image_data = img_file.read()
             command_output = process_image(image_data)
@@ -313,17 +235,14 @@ def product_lookup():
 
         features_file_key = 'image_features.pkl'  # S3 key for the features file
 
-        # Load features from S3
         response = s3.get_object(Bucket=bucket_name, Key=features_file_key)
         features = pickle.loads(response['Body'].read())
 
-        # Construct the payload for the API endpoint
         payload = {
             'bucket_name': bucket_name,
             'img_key': photo_filename
         }
 
-        # Call the external API endpoint
         response = requests.post(api_endpoint, json=payload)
         if response.status_code != 200:
             return jsonify({'error': 'Error calling the API endpoint'}), response.status_code
@@ -360,8 +279,6 @@ def product_lookup():
             random_products_serialized = [product.to_dict() for product in random_products]
             return jsonify(random_products_serialized)
 
-        # if not similar_images:
-
     return jsonify(matched_products)
 
 
@@ -371,13 +288,6 @@ def logout():
     flash('You have been logged out.', 'info')
     return redirect(url_for('login'))
 
-
-# @app.route('/products')
-# def products():
-#     # if 'user' not in session:
-#     #     return redirect(url_for('login'))
-#     return load_products()
-    # return render_template('products.html', products=load_products())
 
 @app.route('/products/categories', methods=['GET'])
 def fetch_categories():
@@ -391,15 +301,11 @@ def fetch_products():
     if category:
         products = Product.query.filter_by(catalog_visibility=True).join(product_categories).join(Category).filter(Category.slug == category).all()
     else:
-        products = Product.query.filter_by(catalog_visibility=True).all()  # Filter products by catalog_visibility
-
-    # if category_id:
-    #     products = get_products_by_category(category_id, products)  # Ensure this function returns serialized data
+        products = Product.query.filter_by(catalog_visibility=True).all()
 
     if include_ids:
-        products = get_products_by_ids(include_ids, products)  # Ensure this function returns serialized data
+        products = get_products_by_ids(include_ids, products)
 
-    # Serialize products using to_dict method
     products_serialized = [product.to_dict() for product in products]
     return jsonify(products_serialized)
 
@@ -430,7 +336,7 @@ def add_product_comment(product_id):
 
     result = response.json()
     if result.get("is_offensive", [1])[0]:
-        # Comment is allowed, add it to the database
+        # Comment is allowed, adding to the db
         comment = Comment(content=content, product_id=product.id)
         db.session.add(comment)
         db.session.commit()
@@ -444,19 +350,11 @@ def add_product_comment(product_id):
 
         return jsonify(response_data), 201
 
-        # return jsonify(comment.to_dict(), result), 201
     else:
         # Comment is not allowed
         response_data = {"error": "Comment is not allowed"}
         response_data.update(result)
         return jsonify(response_data), 400
-        # return jsonify({"error": "Comment is not allowed"}, result), 400
-
-    # comment = Comment(content=content, product_id=product.id)
-    # db.session.add(comment)
-    # db.session.commit()
-
-    # return jsonify(comment.to_dict()), 201
 
 @app.route('/products/<int:product_id>/comments', methods=['GET'])
 def fetch_product_comments(product_id):
@@ -468,26 +366,47 @@ def fetch_product_comments(product_id):
     return jsonify(comments)
 
 
-
 @app.route('/products/<int:product_id>', methods=['GET'])
 def fetch_product_by_id(product_id):
     product = get_product_by_id(product_id)
     if product is None:
         return jsonify({'error': 'Id not found'}), 404
-    return jsonify(product.to_dict())  # Assuming to_dict is implemented in the Id model
+    return jsonify(product.to_dict())  #
 
 
 @app.route('/api/image-preprocessing', methods=['OPTIONS', 'POST'])
 def image_preprocessing():
     return jsonify({'Error': 'Error preprocessing images: An error occurred while trying to preprocess the images. Please try again later.\n For more details, visit our GitHub repository: https://github.com/orcasecurity-research/image-preprocessing-ai-goat'}), 500
 
-# @app.route('/products/suggestions', methods=['GET'])
-# def fetch_product_suggestions():
-#     products = load_products()
-#     # Select 4 random products for suggestion
-#     suggested_products = random.sample(products, 4)
-#     return jsonify(suggested_products)
+@app.route('/hints/challenge1/1', methods=['GET'])
+def hint_1_1():
+    return "Look for some errors"
+@app.route('/hints/challenge1/2', methods=['GET'])
+def hint_1_2():
+    return "Have you heard about malicious PNGs?"
 
+@app.route('/hints/challenge1/3', methods=['GET'])
+def hint_1_3():
+    return "SSRF - Super Stuffed Rainbow Fluff"
+@app.route('/hints/challenge2/1', methods=['GET'])
+def hint_2_1():
+    return f'{args.data_poisoning_bucket}'
+
+@app.route('/hints/challenge2/2', methods=['GET'])
+def hint_2_2():
+    return "1,3,4, Let's explore the fluffy store!"
+@app.route('/hints/challenge2/3', methods=['GET'])
+def hint_2_3():
+    return '--acl bucket-owner-full-control'
+@app.route('/hints/challenge3/1', methods=['GET'])
+def hint_3_1():
+    return 'What is the API output?'
+@app.route('/hints/challenge3/2', methods=['GET'])
+def hint_3_2():
+    return "If you hear a BURP, it's probably just the teddy bears having a tea party in their SUITE"
+@app.route('/hints/challenge3/3', methods=['GET'])
+def hint_3_3():
+    return 'When an answer is provided, is there a point in asking a question?'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
