b50fabf27ba21e08d68b13da977157e67193c537
GangGreenTemperTatum <104169244+GangGreenTemperTatum@users.noreply.github.com>
2024-10-02 19:34:58 -0400

    fix: move backend to tf dir

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket          = "mycomponents-tfstate"
-        key             = "state/terraform.tfstate"
-        region          = "us-east-1"
-        dynamodb_table  = "mycomponents_tf_lockid"
-        encrypt         = true
-    }
-}
