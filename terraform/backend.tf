812e182ec44f421454f2a6f759938df5f53c96a0
GangGreenTemperTatum <104169244+GangGreenTemperTatum@users.noreply.github.com>
2024-10-02 19:48:35 -0400

    fix: fix region in backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket          = "mycomponents-tfstate"
         key             = "state/terraform.tfstate"
-        region          = "us-east-1"
+        region          = "eu-central-1"
         dynamodb_table  = "mycomponents_tf_lockid"
         encrypt         = true
     }
